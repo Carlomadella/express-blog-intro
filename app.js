d00ef27aafa93681ebbe1283f67088ef24e42ffb
Carlomadella <madella871@gmail.com>
2025-04-16 17:50:49 +0200

    [UPD] creato array di oggetti

--- app.js
+++ app.js
@@ -7,12 +7,55 @@ const app = express();
 //  definisco il numero di porta in cui il server rimarrà in ascolto
 const port = 3000;
 
+// impostazione dello strumento per recuperare gli assett statici prima della definizione delle rotte
+app.use(express.static('public'));
+
 // definisco la rotta base della mia app
 app.get("/", (req, res) => {
     console.log("Server del mio blog")
 });
 
+app.get("/blog", (req, res) =>{
+    const blog = [
+        {
+            id: 1,
+            titolo: "Ciambellone",
+            contenuto: "Ciambellone appena sfornato",
+            immagine: "images/ciambellone.jpeg",
+            tags: ["dolce", "secco", "con il buco al centro"]
+        },
+        {
+            id: 2,
+            titolo: "Crackers",
+            contenuto: "Crackers impastati con barbabietole",
+            immagine: "images/crackers_barbabietola.jpeg",
+            tags: ["crackers", "barbabietola", "rosso"]
+        },
+        {
+            id: 3,
+            titolo: "Pane fritto",
+            contenuto: "Pane fatto in casa successivamente fritto",
+            immagine: "images/pane_fritto.jpeg",
+            tags: ["dolce", "pane", "fritto"]
+        },
+        {
+            id: 4,
+            titolo: "Pasta alla barbabietola",
+            contenuto: "Pasta fatta in casa usando la barbabietola",
+            immagine: "images/pasta_barbabietola.jpeg",
+            tags: ["salato", "barbabietola", "rosso"]
+        },
+        {
+            id: 5,
+            titolo: "Torta paesana",
+            contenuto: "Torta paesana fatta in casa",
+            immagine: "images/torta_paesana.jpeg",
+            tags: ["dolce", "barbabietola", "rosso"]
+        },
+    ]
+})
+
 // inserimento del metodo che lascia in ascolto il nostro server
 app.listen(port, () =>{
-    console.log(`Server del nostro ristorante in ascolto alla porta  ${port}`)
-})
+    console.log(`Server del nostro blog in ascolto alla porta  ${port}`)
+});
